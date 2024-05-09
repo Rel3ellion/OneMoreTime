ce19dfca9e5c367a34b140b1266d46268c8afe18
Alexander <jimbox2001@mail.ru>
2024-05-09 19:51:33 +0300

    Fix update

---
+++ Assets/PhysicalButton/Scripts/ButtonShaderController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonShaderController : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+
+
+}
