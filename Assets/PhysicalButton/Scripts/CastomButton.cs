a5ff588503b0e8b4f3601a71252f2cceb5ef30d6
Alexander <jimbox2001@mail.ru>
2024-05-10 07:58:04 +0300

    CastomButton

--- Assets/PhysicalButton/Scripts/CastomButton.cs
+++ Assets/PhysicalButton/Scripts/CastomButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Button : MonoBehaviour
+public class CastomButton : MonoBehaviour
 {
     [SerializeField] private UnityEvent _pressed;
     [SerializeField] private UnityEvent _released;
